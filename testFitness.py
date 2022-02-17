3f54463f481974deed0aaccaec42e4e52b42885a
jana-jovicic <jana.jovicic755@gmail.com>
2022-02-17 22:25:17 +0100

    added mse fitness calculation

---
+++ testFitness.py
@@ -0,0 +1,48 @@
+import random
+from GeneticPrograming.ExpressionTreeManipulation.randomTreeGenerator import generateRandomTree
+from GeneticPrograming.fitness import FitnessFunction
+import numpy as np
+
+from expression import *
+
+
+
+def main():
+
+    X_set0 = np.array([[10,20,30]])
+    X_set1 = np.array([[10,20,30], [40,50,60]])
+
+    # y = (( x0 / x0 ) + ( x2 - x1 ))
+    y_set0 = np.array([11.0])
+    y_set1 = np.array([11.0, 11.0])
+
+    x0 = VariableNode(0)
+    x1 = VariableNode(1)
+    x2 = VariableNode(2)
+
+    # Individual for evaluation: (( x2 / x0 ) + ( x2 - x1 ))
+
+    divNode1 = DivNode()
+    divNode1.appendLeft(x2)
+    divNode1.appendRight(x0)
+
+    subNode1 = SubNode()
+    subNode1.appendLeft(x2)
+    subNode1.appendRight(x1)
+
+    addNode1 = AddNode()
+    addNode1.appendLeft(divNode1)
+    addNode1.appendRight(subNode1)
+    
+    print("Individual for evaluation:")
+    print(addNode1.stringRepresentation())
+    print(addNode1.value(X_set0))
+
+
+    fitness_function = FitnessFunction(X_set1, y_set1)
+
+    fitness_function.Evaluate(addNode1)
+    
+
+if __name__ == "__main__":
+    main()
