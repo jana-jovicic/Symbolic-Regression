5d32e8a877048f7cebef248c1a341fc46108558a
Jana Jovičić <33639887+jana-jovicic@users.noreply.github.com>
2022-03-17 19:05:38 +0100

    bruteForce continued

--- BruteForce/bruteForce.py
+++ BruteForce/bruteForce.py
@@ -7,6 +7,8 @@ from numpy.random import random
 
 from expression import *
 
+
+
 class BruteForce():
 
     def __init__(self,
@@ -17,6 +19,8 @@ class BruteForce():
 		errorType = 'mse',
 		errorEpsilon = 1e-10,
         maxHours = 1):
+
+        
 
         args, _, _, values = inspect.getargvalues(inspect.currentframe())
         values.pop('self')
@@ -30,36 +34,19 @@ class BruteForce():
 
     def run(self):
 
-        treeHeight = 1
-
-        """
-        print(self.X.shape)
-        if self.X.shape[1] > 1:
-            for equationInstancePoints in self.X:
-                xsPermutations = itertools.permutations(equationInstancePoints, 2)
-                for pair in xsPermutations:
-                    print(pair)
-                print('----------')
-        """
-
         terminals = []
-        nextHeightTerminals = []
         nVariables = self.X.shape[1]
         for i in range(nVariables):
             terminals.append(VariableNode(i))
-            nextHeightTerminals.append(VariableNode(i))
         print("terminals", terminals)
         print(type(terminals))
+       
+        self.currentBestSolution = ''
+        self.currentBestSolutionError = np.inf
 
-        """
-        terminalPermutations = []
-        if nVariables > 1:
-            for pair in itertools.product(terminals, repeat=2):
-                terminalPermutations.append(pair)
-                print(pair)
-        """
-
-
+        foundExactSolution = False
+        exactSolution = ''
+        exactSolutionErr = np.inf
 
         height = 0
 
@@ -73,17 +60,22 @@ class BruteForce():
                 terminalPermutations.append(pair)
                 #print("pair:", pair[0].stringRepresentation(), ", ", pair[1].stringRepresentation())
 
-            foundSolution, solution, functions = self.buildTree(terminalPermutations)
-            print('foundSolution', foundSolution)
-            print('solution', solution)
+            foundExactSolution, exactSolution, exactSolutionErr, functions = self.buildTree(terminalPermutations)
+            print('foundSolution', foundExactSolution)
+            if foundExactSolution:
+                print('solution', exactSolution.stringRepresentation())
+                break
             #print('functions', functions)
 
+
+            
             # -----------
             # just for debug
             height += 1
             if height == 3:
                 break
             # ----------
+            
 
             
             for function in functions:
@@ -91,80 +83,38 @@ class BruteForce():
                     continue
                 else:
                     terminals.append(deepcopy(function))
-                    """
-                    print(function.stringRepresentation())
-                    print(terminals[2].stringRepresentation())
-                    print(function == terminals[2])
-                    print(function.stringRepresentation() == terminals[2].stringRepresentation())
-                    print('//////////')
-                    """
+                   
                     
 
-            for term in terminals:
-                print("term", term.stringRepresentation())
+            #for term in terminals:
+                #print("term", term.stringRepresentation())
 
-            """
+            
             terminalPermutations = []
             for pair in itertools.product(terminals, repeat=2):
                     terminalPermutations.append(pair)
-            """
+            
 
             #for pair in terminalPermutations:
-            #    print("pair", pair[0].stringRepresentation(),  pair[1].stringRepresentation())
+                #print("pair", pair[0].stringRepresentation(),  pair[1].stringRepresentation())
 
 
-            print('------------------')
+            #print('------------------')
+
+
+        return foundExactSolution, exactSolution, exactSolutionErr, self.currentBestSolution, self.currentBestSolutionError
         
         
-
-        """
-        # height = 1
-        foundSolution = False
-        solution = ''
-
-        for func in self.functions:
-            print(func)
-            n = len(terminalPermutations)
-            #for pair in terminalPermutations:
-            for i in range(n):
-                pair = terminalPermutations[i]
-                func.appendLeft(deepcopy(pair[0]))
-
-                if func.arity == 1:
-                    #print(pair)
-                    #print(terminalPermutations[i-1])
-                    if pair[0] == terminalPermutations[i-1][0]:
-                        print("same")
-                        continue
-
-                if func.arity > 1:
-                    func.appendRight(deepcopy(pair[1]))
-                print(func.stringRepresentation())
-                y_pred = func.value(self.X)
-                y_pred = [np.inf if np.isnan(y) else y for y in y_pred]
-                print(y_pred[:5])
-                err = self.mse(self.y, y_pred)
-                print("mse", err)
-                if err < self.errorEpsilon:
-                    foundSolution = True
-                    solution = func
-                    break
-
-            if foundSolution:
-                break
-
-        print("solution:", solution)
-        if solution != '':
-            print("solution:", solution.stringRepresentation())
-        """
 
 
     
     def buildTree(self, terminalPermutations):
 
         functions = []
-        foundSolution = False
-        solution = ''
+
+        foundExactSolution = False
+        exactSolution = ''
+        exactSolutionErr = np.inf
 
         for func in self.functions:
             #print(func)
@@ -192,13 +142,25 @@ class BruteForce():
                 #print(y_pred[:5])
                 err = self.mse(self.y, y_pred)
                 #print("mse", err)
-                if err < self.errorEpsilon:
-                    foundSolution = True
-                    solution = func
-                    break
 
-            if foundSolution:
+                if err < self.currentBestSolutionError:
+                    self.currentBestSolution = func
+                    self.currentBestSolutionError = err
+                    
+                print(self.currentBestSolution.stringRepresentation())
+                print(self.currentBestSolutionError)
+
+                if err < self.errorEpsilon:
+                    foundExactSolution = True
+                    exactSolution = func
+                    exactSolutionErr = err
+                    #self.currentBestSolutionError = err
+                    #self.currentBestSolution = func
+                    break
+            
+
+            if foundExactSolution:
                 break
 
-        return foundSolution, solution, functions
+        return foundExactSolution, exactSolution, exactSolutionErr, functions
             
